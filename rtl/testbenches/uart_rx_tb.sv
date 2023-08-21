a221ec7022f133f6aadede13751e670dde6f313a
Ethan Peterson <ethan@petetech.net>
2023-08-20 21:42:51 -0700

    Basic Rx Testbench

--- rtl/testbenches/uart_rx_tb.sv
+++ rtl/testbenches/uart_rx_tb.sv
@@ -1,5 +1,5 @@
-
 `default_nettype none
+`timescale 1ns / 1ps
 
 `include "vunit_defines.svh"
 
@@ -8,29 +8,85 @@ module uart_rx_tb;
 
     var logic rx_clk;
     var logic sys_clk;
-
+    var logic rxd = 1;
+    var logic[7:0] test_value = 10;
     always begin
         // wait uneven amount of time so that we can test synchronization
         // behavior
-        #10001
+        #2
         rx_clk <= !rx_clk;
     end
 
     always begin
-        #10
+        #1
         sys_clk <= !sys_clk;
     end
 
+    typedef enum int {
+        SEND_START_BIT,
+        SEND_TEST_DATA,
+        SEND_STOP_BIT
+    } uart_rx_tb_state_t;
+
+    uart_rx_tb_state_t state = SEND_START_BIT;
+    var logic [3:0] bit_count = 0;
+    always @(posedge rx_clk) begin
+        case (state)
+            SEND_START_BIT: begin
+                rxd <= 0;
+                state <= SEND_TEST_DATA;
+            end
+
+            SEND_TEST_DATA: begin
+                if (bit_count < 7) begin
+                    rxd <= test_value[bit_count];
+                    bit_count <= bit_count + 1;
+                end else begin
+                    bit_count <= 0;
+                    state <= SEND_STOP_BIT;
+                end
+            end
+
+            SEND_STOP_BIT: begin
+                rxd <= 1'b1;
+                // wait a cycle and send the start bit again
+                state <= SEND_START_BIT;
+            end
+        endcase
+    end
+
+    axis_interface rx_stream (
+        .clk(sys_clk),
+        .reset(0)
+    );
+
+    uart_rx #(
+        .CLKS_PER_BIT(2)
+    ) DUT (
+        .rxd(rxd),
+        .stream(rx_stream)
+    );
+
+    var logic [7:0] iter = 0;
     // Begin the Test suite run
     `TEST_SUITE begin
         `TEST_SUITE_SETUP begin
             // what would normally go in an initial block we can put here
             rx_clk = 0;
             sys_clk = 0;
+            rx_stream.tready = 1;
         end
 
-        `TEST_CASE("example") begin
-            @(posedge rx_clk) `CHECK_EQUAL(0, 0);
+        `TEST_CASE("check_data_integrity") begin
+            automatic int bytes_consumed = 0;
+            while (bytes_consumed < 1) begin
+                @(posedge sys_clk) begin
+                    if (rx_stream.tvalid) begin
+                        `CHECK_EQUAL(rx_stream.tdata, test_value);
+                        bytes_consumed = bytes_consumed + 1;
+                    end
+                end
+            end
         end
     end
 
