9924c56ded0a9abf7a106718e9b04b928c28de05
Ethan Peterson <ethan@petetech.net>
2023-09-23 17:32:05 -0700

    add MDIO interface

--- rtl/src/ethernet/mdio/mdio_interface.sv
+++ rtl/src/ethernet/mdio/mdio_interface.sv
@@ -2,6 +2,19 @@
 `timescale 1ns / 1ps
 
 interface axis_interface ();
+var logic mdc;
+var logic mdio;
+
+modport Master (
+    output mdc,
+    inout mdio
+);
+
+modport Slave (
+    input mdc,
+    inout mdio
+);
+
 
 endinterface // mdio_interface
 
