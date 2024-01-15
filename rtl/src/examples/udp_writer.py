5263c45f9cd1706a3cd5f5cab92d35dd2b58e308
Ethan Peterson <ethan@petetech.net>
2024-01-14 21:52:55 -0800

    Finish udp rx example

---
+++ rtl/src/examples/udp_writer.py
@@ -0,0 +1,14 @@
+# NOTE: This script should be used to test the udp_tx_example module.
+# This script will print the byte integer that is sent in a loop by the FPGA
+
+# Modified example from https://wiki.python.org/moin/UdpCommunication
+import socket
+
+UDP_IP = "192.168.1.128"
+UDP_PORT = 3000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, 25, str("enp56s0u2u1" + '\0').encode('utf-8'))
+
+data = 7
+sock.sendto(b'A', (UDP_IP, UDP_PORT))
