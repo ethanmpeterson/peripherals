d061ea7611de500b3dda7b2128f0088d438cc9b7
Ethan Peterson <ethan@petetech.net>
2024-01-15 11:36:13 -0800

    Interface for axi_lite

--- rtl/src/axi_lite/axi_lite_interface.sv
+++ rtl/src/axi_lite/axi_lite_interface.sv
@@ -19,7 +19,6 @@ interface axi_lite_interface #(
     // Address read signals
     var logic[READ_ADDRESS_WIDTH-1:0] araddr;
     var logic[2:0] arprot;
-
     var logic arvalid;
     var logic arready;
 
@@ -38,11 +37,103 @@ interface axi_lite_interface #(
     // Write data channel
     var logic[WRITE_DATA_WIDTH-1:0] wdata;
     var logic[STROBE_WIDTH-1:0] wstrb;
+    var logic wvalid;
+    var logic wready;
 
     // Write response channel
     var logic[1:0] bresp;
     var logic bvalid;
     var logic bready;
+
+    modport Master (
+        output awaddr,
+        output awprot,
+        output awvalid,
+        input awready,
+
+
+        output wdata,
+        output wstrb,
+        output wvalid,
+        input wready,
+
+        input bresp,
+        input bvalid,
+        output bready,
+
+
+        output araddr,
+        output arprot,
+        output arvalid,
+        input arready,
+
+
+        input rdata,
+        input rresp,
+        input rvalid,
+        output rready
+    );
+
+
+    modport Slave (
+        input awaddr,
+        input awprot,
+        input awvalid,
+        output awready,
+
+
+        input wdata,
+        input wstrb,
+        input wvalid,
+        output wready,
+
+        output bresp,
+        output bvalid,
+        input bready,
+
+
+        input araddr,
+        input arprot,
+        input arvalid,
+        output arready,
+
+
+        output rdata,
+        output rresp,
+        output rvalid,
+        input rready
+    );
+
+
+    modport Monitor (
+        input awaddr,
+        input awprot,
+        input awvalid,
+        input awready,
+
+
+        input wdata,
+        input wstrb,
+        input wvalid,
+        input wready,
+
+
+        input bresp,
+        input bvalid,
+        input bready,
+
+
+        input araddr,
+        input arprot,
+        input arvalid,
+        input arready,
+
+
+        input rdata,
+        input rresp,
+        input rvalid,
+        input rready
+    );
 
 endinterface // axi_lite_interface
 
